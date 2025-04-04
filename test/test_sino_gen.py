89ff3f76ca058f8190c2aa9b3e6b9c316d32cfd7
marischan888 <marischancql@gmail.com>
2025-04-04 13:14:38 -0400

    update test

--- test/test_sino_gen.py
+++ test/test_sino_gen.py
@@ -2,11 +2,6 @@ import numpy as np
 import pytest
 from src.core.sino_gen import SinoGen
 
-def test_sinogen_valid():
-    dummy_image = np.ones((64, 64))
-    sg = SinoGen(dummy_image, theta=180)
-    result = sg.radon()
-    assert result.shape[1] == 180
 
 def test_sinogen_invalid_input():
     with pytest.raises(ValueError):
